51536eec5f09dbe514a60f94ef05192f6f0364b1
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-06-13 09:32:30 +0900

    4일 차 커밋

---
+++ Day04/rgb_blink.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+red =17
+green = 27
+blue = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red,GPIO.OUT)
+GPIO.setup(green,GPIO.OUT)
+GPIO.setup(blue,GPIO.OUT)
+
+# 초기화
+GPIO.output(green, True) 
+GPIO.output(blue, True)
+GPIO.output(red, True)
+        
+
+try:
+    while True:
+        # RED
+        GPIO.output(green, True)   
+        GPIO.output(blue, True)
+        GPIO.output(red, False)
+        time.sleep(2)
+        # GREEN
+        GPIO.output(blue, True)
+        GPIO.output(red, True)
+        GPIO.output(green, False)
+        time.sleep(2)
+
+        # BLUE
+        GPIO.output(red, True)
+        GPIO.output(green, True)
+        GPIO.output(blue, False)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    GPIO.output(green, True)   
+    GPIO.output(blue, True)
+    GPIO.output(red, True)
+
+    GPIO.cleanup()
+
